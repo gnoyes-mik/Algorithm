8ca6580792bd9b5675004e7f7d88680987645b99
jeff-seyong <sedragon.kim@gmail.com>
2020-07-26 20:10:45 +0900

    Programmers 2016년

---
+++ src/P0004.java
@@ -0,0 +1,40 @@
+// Programmers 2016년
+public class P0004 {
+    public static String solution(int a, int b) {
+        String answer = "";
+        int[] dayOfMonth = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int sumOfDay = 0;
+        for (int i = 0; i < a - 1; i++) {
+            sumOfDay += dayOfMonth[i];
+        }
+        sumOfDay += b;
+        switch (sumOfDay % 7) {
+            case 0:
+                answer = "THU";
+                break;
+            case 1:
+                answer = "FRI";
+                break;
+            case 2:
+                answer = "SAT";
+                break;
+            case 3:
+                answer = "SUN";
+                break;
+            case 4:
+                answer = "MON";
+                break;
+            case 5:
+                answer = "TUE";
+                break;
+            case 6:
+                answer = "WED";
+                break;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
