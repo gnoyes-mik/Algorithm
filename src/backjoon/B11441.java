76d7c8f44e333a1172a5a4ece161ac6b5acf9c91
jeff-seyong <sedragon.kim@gmail.com>
2020-07-28 00:03:08 +0900

    합 구하기

---
+++ src/backjoon/B11441.java
@@ -0,0 +1,28 @@
+package backjoon;
+
+import java.util.Scanner;
+
+public class B11441 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int[] arr = new int[N + 1];
+        int[] sumArr = new int[N + 1];
+
+        for (int i = 0; i < arr.length; i++) {
+            if (i == 0) {
+                //empty
+            } else {
+                arr[i] = sc.nextInt();
+                sumArr[i] = sumArr[i - 1] + arr[i];
+            }
+        }
+        int M = sc.nextInt();
+        for (int j = 0; j < M; j++) {
+            int start = sc.nextInt();
+            int end = sc.nextInt();
+            System.out.println(sumArr[end] - sumArr[start - 1]);
+        }
+    }
+}
