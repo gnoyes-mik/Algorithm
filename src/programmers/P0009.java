3119575a0e9d1641faed689bc8a453631d3e782b
jeff-seyong <sedragon.kim@gmail.com>
2020-07-28 17:51:13 +0900

    합 구하기

---
+++ src/programmers/P0009.java
@@ -0,0 +1,24 @@
+package programmers;
+
+// 문자열 내 p와 y의 개수
+public class P0009 {
+    boolean solution(String s) {
+        boolean answer = false;
+        int count = 0;
+
+        s = s.toLowerCase();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'p') {
+                count++;
+            } else if (s.charAt(i) == 'y') {
+                count--;
+            }
+        }
+        if (count == 0) {
+            answer = true;
+        }
+
+        return answer;
+    }
+}
