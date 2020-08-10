f4574bf9c05a9d0f9e47f80ca149c0916a8a2bf0
jeff-seyong <sedragon.kim@gmail.com>
2020-08-10 14:05:25 +0900

    약수의 합

---
+++ src/programmers/P0014.java
@@ -0,0 +1,22 @@
+package programmers;
+
+// 약수의 합
+public class P0014 {
+    public int solution(int n) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        }
+
+        int answer = 1 + n;
+
+        for (int i = 2; i <= n / 2; i++) {
+            if (n % i == 0) {
+                answer += i;
+            }
+        }
+
+        return answer;
+    }
+}
