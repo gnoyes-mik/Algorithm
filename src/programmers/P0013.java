694d0475392f7faf5f36a899c675aacb5033b926
jeff-seyong <sedragon.kim@gmail.com>
2020-08-10 14:05:14 +0900

    시저 암호

---
+++ src/programmers/P0013.java
@@ -0,0 +1,21 @@
+package programmers;
+
+// 시저 암호
+public class P0013 {
+    public String solution(String s, int n) {
+        String answer = "";
+        int len = s.length();
+        char alpha;
+        char start;
+
+        for (int i = 0; i < len; i++) {
+            alpha = s.charAt(i);
+            if (alpha != ' ') {
+                start = Character.isLowerCase(alpha) ? 'a' : 'A';
+                alpha = (char) (start + (alpha + n - start) % 26);
+            }
+            answer += alpha;
+        }
+        return answer;
+    }
+}
