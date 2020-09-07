49abd3299748cf79e827481420844a1bcb070b6c
jeff-seyong <sedragon.kim@gmail.com>
2020-09-07 16:03:20 +0900

    200907

--- src/baekjoon/step/다이나믹프로그래밍/B11052_카드구매하기.java
+++ src/baekjoon/step/다이나믹프로그래밍/B11052_카드구매하기.java
@@ -18,7 +18,6 @@ public class B11052_카드구매하기 {
     public static void buyCard(int n) {
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.println("D["+i+"] = max(D["+i+"], D["+i+" - "+j+"] + P["+j+"]) = max("+D[i]+", "+D[i - j]+" + "+P[j]+")");
                 D[i] = max(D[i], D[i - j] + P[j]);
             }
         }
