9ffca6cf8262c13e2c143e4c5b63081cc487ca33
jeff-seyong <sedragon.kim@gmail.com>
2020-10-24 13:31:36 +0900

    201024

---
+++ src/programmers/level2/S2020_가장큰수.java
@@ -0,0 +1,28 @@
+package programmers.level2;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class S2020_가장큰수 {
+
+    public static String solution(int[] numbers) {
+        String[] sArr = new String[numbers.length];
+
+        for (int i = 0; i < numbers.length; i++) {
+            sArr[i] = String.valueOf(numbers[i]);
+        }
+        Arrays.sort(sArr, new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                return (s2 + s1).compareTo(s1 + s2);
+            }
+        });
+
+        return sArr[0].equals("0") ? "0" : String.join("", sArr);
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(solution(new int[]{6, 10, 2}));
+    }
+}
