3dde6e3a1bc88c919a887bfbfeba0e6df4a2aced
jeff-seyong <sedragon.kim@gmail.com>
2020-11-05 16:35:46 +0900

    201105

---
+++ src/baekjoon/step/백트래킹/N과M/N과M_5.java
@@ -0,0 +1,50 @@
+package baekjoon.step.백트래킹.N과M;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * Q N개의 자연수와 자연수 M이 주어졌을 때, 아래 조건을 만족하는 길이가 M인 수열을 모두 구하는 프로그램을 작성하시오. N개의 자연수는 모두 다른 수이다.
+ * - N개의 자연수 중에서 M개를 고른 수열
+ * - 출력은 사전 순으로 증가하는 순서로
+ */
+public class N과M_5 {
+    static int N, M;
+    static int[] arr;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        visited = new boolean[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        permutation(0, "");
+
+    }
+
+    private static void permutation(int cnt, String val) {
+        if (cnt == M) {
+            System.out.println(val);
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                permutation(cnt + 1, val + arr[i] + " ");
+                visited[i] = false;
+            }
+        }
+    }
+}
