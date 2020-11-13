0f48ce413a9da22c2e338007af0e1a86baca9384
jeff-seyong <sedragon.kim@gmail.com>
2020-11-13 13:46:48 +0900

    add Utils.java

---
+++ src/sorting/Utils.java
@@ -0,0 +1,16 @@
+package sorting;
+
+public class Utils {
+    public static int[] swapValue(int[] arr, int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+        return arr;
+    }
+
+    public static char[] swapValue(char[] arr, int a, int b) {
+        char temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+        return arr;
+    }}
